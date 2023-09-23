b59c34124d9b754edf9856e9aeef094a429c0fc8
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-09-23 17:30:54 -0400

    Create Sept24.java

---
+++ .LeetcodeDailySolution/Sept24.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int lastStoneWeightII(int[] stones) {
+        int totalWeight = 0;
+        for (int stone : stones) {
+            totalWeight += stone;
+        }
+        int maxWeight = totalWeight / 2;
+
+        boolean[] dp = new boolean[maxWeight + 1];
+        dp[0] = true;
+
+        for (int stone : stones) {
+            for (int j = maxWeight; j >= stone; j--) {
+                dp[j] = dp[j] || dp[j - stone];
+            }
+        }
+
+        for (int j = maxWeight; j >= 0; j--) {
+            if (dp[j]) {
+                return totalWeight - 2 * j;
+            }
+        }
+
+        return 0;
+    }
+}
