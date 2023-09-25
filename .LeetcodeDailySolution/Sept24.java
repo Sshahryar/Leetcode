d92297ebc36ce582908ecf50e1a738d093c7b812
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-09-24 20:52:18 -0400

    Create Sept24.java

---
+++ .LeetcodeDailySolution/Sept24.java
@@ -0,0 +1,33 @@
+/* Sept 25, 2023
+*
+*/
+
+public class Solution {
+    public char findTheDifference(String s, String t) {
+        int[] charCount = new int[26];
+        
+        for (char c : s.toCharArray()) {
+            charCount[c - 'a']++;
+        }
+        
+        for (char c : t.toCharArray()) {
+            charCount[c - 'a']--;
+        }
+        
+        for (int i = 0; i < 26; i++) {
+            if (charCount[i] < 0) {
+                return (char) (i + 'a');
+            }
+        }
+        
+        return ' ';
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "abcd";
+        String t = "abcde";
+        char addedLetter = solution.findTheDifference(s, t);
+        System.out.println("The added letter is: " + addedLetter);
+    }
+}
