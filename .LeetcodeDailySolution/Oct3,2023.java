9871c43c806d3deb81a7bb8ea7124a1b0c8a921c
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-10-03 19:03:47 -0400

    Create Oct3,2023.java

---
+++ .LeetcodeDailySolution/Oct3,2023.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numIdenticalPairs(int[] nums) {
+        int[] count = new int[101]; 
+
+        for (int num : nums) {
+            count[num]++;
+        }
+        
+        int goodPairs = 0;
+        
+        for (int c : count) {
+            if (c > 1) {
+                goodPairs += (c * (c - 1)) / 2;
+            }
+        }
+        
+        return goodPairs;
+    }
+}
