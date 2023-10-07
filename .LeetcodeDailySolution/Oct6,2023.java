9d5b34e65340ce90f3194733bb45bcfa4dedd26c
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-10-07 15:38:57 -0400

    Create Oct6,2023.java

---
+++ .LeetcodeDailySolution/Oct6,2023.java
@@ -0,0 +1,13 @@
+class Solution {
+        public int integerBreak(int n) {
+                if(n == 1) return 1;
+                        int[] dp = new int[n + 1];
+                                dp[1] = 1;
+                                        for(int i = 2; i <= n; i++){
+                                                    for(int j = 1; j < i; j++){
+                                                                    dp[i] = Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+                                                                                }
+                                                                                        }
+                                                                                                return dp[n];
+                                                                                                    }
+                                                                                                    }
