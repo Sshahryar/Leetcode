2cefed49bff6f45b2cc1c3ce49e344031ca00b51
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-11-02 19:53:32 -0400

    Create Nov1,2023.java

---
+++ .LeetcodeDailySolution/Nov1,2023.java
@@ -0,0 +1,41 @@
+class Solution {
+    private int currentVal;
+    private int currentCount = 0;
+    private int maxCount = 0;
+    private int modeCount = 0;
+    private int[] modes;
+    public int[] findMode(TreeNode root) {
+        inOrderTraversal(root);
+
+        modes = new int[modeCount];
+        modeCount = 0;
+        currentCount = 0;
+
+        inOrderTraversal(root);
+
+        return modes;
+    }
+    private void inOrderTraversal(TreeNode node) {
+        if (node == null) return;
+        inOrderTraversal(node.left);
+        handleValue(node.val);
+        inOrderTraversal(node.right);
+    }
+    private void handleValue(int val) {
+        if (val == currentVal) {
+            currentCount++;
+        } else {
+            currentVal = val;
+            currentCount = 1;
+        }
+        if (currentCount > maxCount) {
+            maxCount = currentCount;
+            modeCount = 1;
+        } else if (currentCount == maxCount) {
+            if (modes != null) {
+                modes[modeCount] = currentVal;
+            }
+            modeCount++;
+        }
+    }
+}
