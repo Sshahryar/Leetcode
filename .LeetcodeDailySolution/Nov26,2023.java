20303126e578f3c5e8c0ee2686ac60cc87dac7d2
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-11-26 21:37:17 -0500

    Create Nov26,2023.java

---
+++ .LeetcodeDailySolution/Nov26,2023.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int largestSubmatrix(int[][] matrix) {
+        int row = matrix.length;
+        int col = matrix[0].length;
+
+        for (int i = 1; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (matrix[i][j] == 1) {
+                    matrix[i][j] += matrix[i - 1][j];
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 0; i < row; i++) {
+            Arrays.sort(matrix[i]);
+
+            for (int j = 0; j < col; j++) {
+                int height = matrix[i][j];
+                int width = col - j;
+                res = Math.max(res, height * width);
+            }
+        }
+
+        return res;        
+    }
+}
