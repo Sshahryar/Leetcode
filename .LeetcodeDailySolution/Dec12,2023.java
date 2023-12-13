fd8b8c60aa9cdab67f785ef329aa7c4cf3c715d7
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2023-12-12 21:18:09 -0500

    Create Dec12,2023.java

---
+++ .LeetcodeDailySolution/Dec12,2023.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int firstMax = 0;
+        int secondMax = 0;
+
+        for (int num : nums) {
+            if (num > firstMax) {
+                secondMax = firstMax;
+                firstMax = num;
+            } else {
+                secondMax = Math.max(secondMax, num);
+            }
+        }
+        return (firstMax - 1) * (secondMax - 1);        
+    }
+}
