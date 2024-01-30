0e5bd56c78ce1d25e6261790ec2408f67d0f0b64
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-01-30 14:43:00 -0500

    Create Jan30,2024.cpp

---
+++ .LeetcodeDailySolution/Jan30,2024.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> numbers;
+
+        for (const string& token : tokens) {
+            if (token.size() > 1 || isdigit(token[0])) {
+                numbers.push(stoi(token));
+            } else { 
+                int operand2 = numbers.top();
+                numbers.pop();
+
+                int operand1 = numbers.top();
+                numbers.pop();
+
+                switch (token[0]) {
+                    case '+': 
+                        numbers.push(operand1 + operand2);
+                        break;
+                    case '-': 
+                        numbers.push(operand1 - operand2);
+                        break;
+                    case '*': 
+                        numbers.push(operand1 * operand2);
+                        break;
+                    case '/': 
+                        numbers.push(operand1 / operand2);
+                        break;
+                }                
+            }
+        }
+        return numbers.top();
+    }
+};
