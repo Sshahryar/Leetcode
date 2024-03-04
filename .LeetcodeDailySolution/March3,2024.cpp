7dd6f1f795a29286c278301eaaf494a883ed7322
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-03-03 20:36:39 -0500

    Create March3,2024.cpp

---
+++ .LeetcodeDailySolution/March3,2024.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+
+    ListNode *temp,*prev;
+
+    int node=0,count=1;
+
+    temp=head;
+
+    while(temp){
+        temp=temp->next;
+        node++;
+    }
+    if(node-n==0){
+        head=head->next;
+        return head;
+    }
+    temp=head;
+
+    while(count!=node-n+1){
+        prev=temp;
+        temp=temp->next;
+        count++;
+    }
+    prev->next=temp->next;
+
+    temp->next=NULL;
+    
+    return head;
+    }
+};
