1fbbcc7f3b3a5157752000b3c9639f3592728510
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-03-13 15:07:21 -0400

    Create March13,2024.cpp

---
+++ .LeetcodeDailySolution/March13,2024.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int pivotInteger(int n) {
+      int ls = (n * (n + 1)) /2, rs = 0;
+
+      while(ls > rs){
+        rs += n;
+        
+        if(rs == ls) return n;
+        ls -= n;
+        n--;
+      }
+      return -1;
+    }
+};
