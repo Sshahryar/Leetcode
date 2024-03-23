a19bda610e3cfdf00cc62bca91628b98f4f23cf7
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-03-22 20:45:00 -0400

    Create March22,2024.cpp

---
+++ .LeetcodeDailySolution/March22,2024.cpp
@@ -0,0 +1,21 @@
+int init = [] {
+
+    ios_base::sync_with_stdio(false);  cin.tie(nullptr);
+    ofstream out("user.out");
+
+    for (string s; getline(cin, s);)
+        out << (equal(s.begin() + 1, s.begin()+s.size()/2, s.rbegin() + 1) ? "true\n" : "false\n");
+
+    out.flush();
+
+    exit(0);
+
+    return 0;
+}();
+class Solution {
+public:
+    bool isPalindrome(ListNode* tail) {
+
+        return true;
+    }
+};
