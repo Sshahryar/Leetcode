58e24c1889d570ab37ece077dbfcfc1f8f09fb10
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-03-24 18:39:42 -0400

    Create March24,2024.cpp

---
+++ .LeetcodeDailySolution/March24,2024.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+    
+        while(nums[0] != nums[nums[0]]){
+
+        swap(nums[0], nums[nums[0]]);
+    }
+    return nums[0];
+    }
+};
