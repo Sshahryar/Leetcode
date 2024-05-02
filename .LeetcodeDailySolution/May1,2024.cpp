58c150030ee6de56f1bcc0fac6a3e6e528aad119
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-05-01 21:36:28 -0400

    Create May1,2024.cpp

---
+++ .LeetcodeDailySolution/May1,2024.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    string reversePrefix(string word, char ch) {
+        int ind = word.find(ch);
+
+        if(ind != string::npos){
+            reverse(word.begin(), word.begin() + ind + 1);
+        }
+        return word;
+    }
+};
