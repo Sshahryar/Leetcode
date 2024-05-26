832b6af310a1adb88bbb3e7d4251297a9703eaeb
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-05-25 20:34:07 -0400

    Create May25,2024.cpp

---
+++ .LeetcodeDailySolution/May25,2024.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    unordered_map<string, vector<string>> dp;
+
+    vector<string> dfs(string s, vector<string>& wordDict) {
+
+        if (dp.count(s) > 0) {
+            return dp[s];
+        }
+
+        vector<string> ans;
+
+        if (s.empty()) {
+
+            ans.push_back("");
+            return ans;
+        }
+
+        for (string& w : wordDict) {
+
+            int sz = w.size();
+
+            if (s.substr(0, sz) == w) {
+
+                vector<string> rest = dfs(s.substr(sz), wordDict);
+
+                for (string& r : rest) {
+
+                    ans.push_back(w + (r.empty() ? "" : " ") + r);
+                }
+            }
+        }
+        return dp[s] = ans;
+        ;
+    }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+
+        return dfs(s, wordDict);
+    }
+};
