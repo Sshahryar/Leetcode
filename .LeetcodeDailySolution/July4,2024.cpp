6819745e0c7212d9a19308596b47411c289d0973
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-07-04 16:33:11 -0400

    Create July4,2024.cpp

---
+++ .LeetcodeDailySolution/July4,2024.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+
+        ListNode* modify = head -> next; 
+        ListNode* nextSum = modify;
+
+        while (nextSum != nullptr) {
+
+            int sum = 0;
+
+            while (nextSum -> val != 0) {
+                sum += nextSum -> val;
+                nextSum = nextSum -> next;
+            }
+
+
+            modify -> val = sum;
+            nextSum = nextSum -> next;
+            modify -> next = nextSum;
+            modify = modify -> next;
+
+        }
+
+        return head -> next;
+    }
+};
