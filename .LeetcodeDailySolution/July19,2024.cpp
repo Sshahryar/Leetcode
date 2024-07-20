c02567c54a854f5f1aeb550e8c5ba44c1867e21c
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-07-19 21:46:12 -0400

    Create July19,2024.cpp

---
+++ .LeetcodeDailySolution/July19,2024.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> luckyNumbers(vector<vector<int>>& matrix) {
+
+        if (matrix.empty() || matrix[0].empty())
+            return {};
+
+        int m = matrix.size(), n = matrix[0].size();
+
+        vector<int> rowMin(m, INT_MAX);
+        vector<int> colMax(n, INT_MIN);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rowMin[i] = min(rowMin[i], matrix[i][j]);
+                colMax[j] = max(colMax[j], matrix[i][j]);
+            }
+        }
+
+        vector<int> result;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == rowMin[i] && matrix[i][j] == colMax[j]) {
+                    result.push_back(matrix[i][j]);
+                }
+            }
+        }
+
+        return result;
+    }
+};
