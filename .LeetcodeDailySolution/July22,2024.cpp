53b0fc32247cb8aac3dafa49a5c6c8b1851f81a2
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-07-22 21:47:55 -0400

    Create July22,2024.cpp

---
+++ .LeetcodeDailySolution/July22,2024.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    static vector<string> sortPeople(vector<string>& names,
+                                     vector<int>& heights) {
+  
+        const int n = names.size();
+
+        for (int i = 0; i < n; i++)
+            heights[i] = (heights[i] << 10) + i;
+
+        sort(heights.begin(), heights.end(), greater<>());
+
+        vector<string> ans(n);
+
+        for (int i = 0; i < n; i++)
+            ans[i] = names[heights[i] & 1023];
+            
+        return ans;
+    }
+};
