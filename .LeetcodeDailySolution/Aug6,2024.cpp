456e25fb2573958cd7e56264e74fc71b1c20fec7
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-08-07 22:06:47 -0400

    Create Aug6,2024.cpp

---
+++ .LeetcodeDailySolution/Aug6,2024.cpp
@@ -0,0 +1,30 @@
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+
+        vector<int> freq(26, 0);
+
+        for (char c : word) {
+            freq[c - 'a']++;
+        }
+        
+        sort(freq.begin(), freq.end());
+        
+        int totalPushes = 0;
+        int multiplier = 1;
+        
+        for (int i = 25; i >= 0; i--) {
+            
+            if (freq[i] == 0) break;
+            
+            if ((25 - i) % 8 == 0 && i != 25) {
+                multiplier++;
+            }
+            
+            totalPushes += freq[i] * multiplier;
+        }
+        
+        return totalPushes;
+    }
+};
