4df7162986549abe9593cd122aafa4b25ef14b45
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-08-12 22:08:30 -0400

    Create Aug12,2024.cpp

---
+++ .LeetcodeDailySolution/Aug12,2024.cpp
@@ -0,0 +1,24 @@
+class KthLargest {
+public:
+
+    int k;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    KthLargest(int k, vector<int>& nums) : k(k) {
+
+        for (int x : nums) {
+            pq.push(x);
+
+            if (pq.size() > k)
+                pq.pop();
+        }
+    }
+    int add(int val) {
+        pq.push(val);
+
+        if (pq.size() > k)
+            pq.pop();
+
+        return pq.top();
+    }
+};
