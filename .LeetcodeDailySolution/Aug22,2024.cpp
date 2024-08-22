4a5aad88ce2d6969984c6bdd0984332133da305b
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-08-22 16:29:39 -0400

    Create Aug22,2024.cpp

---
+++ .LeetcodeDailySolution/Aug22,2024.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    static int findComplement(int num) {
+
+        if (num == 1)
+            return 0;
+
+        int ans = 0;
+
+        for (int b = 0; num; b++, num >>= 1) { 
+            ans += (1 - (num & 1)) << b;       
+        }
+        
+        return ans;
+    }
+};
