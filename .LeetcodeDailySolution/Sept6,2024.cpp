43d8e44a2f323f378d734f57a6330390a2694034
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-09-06 23:58:04 -0400

    Create Sept6,2024.cpp

---
+++ .LeetcodeDailySolution/Sept6,2024.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode* modifiedList(vector<int>& nums, ListNode* head) {
+
+        bitset<100001> hasN = 0;
+
+        for (int x : nums)
+            hasN[x] = 1;
+
+        ListNode dummy(0, head);
+        ListNode *prev = &dummy, *tmp = NULL;
+
+        for (ListNode* curr = head; curr; curr = curr -> next, delete tmp) {
+            if (hasN[curr -> val]) {
+                prev -> next = curr -> next;
+                tmp = curr;
+            } else {
+                prev = prev->next;
+                tmp = NULL;
+            }
+        }
+
+        return dummy.next;
+    }
+};
