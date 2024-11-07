5b764e8e8ac5fd939536cd72c1893dec7b0c3db3
Sshahryar <123003299+Sshahryar@users.noreply.github.com>
2024-11-06 23:50:41 -0500

    Create Nov6,2024.cpp

---
+++ .LeetcodeDailySolution/Nov6,2024.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canSortArray(std::vector<int>& nums) {
+
+        int prevMax = INT_MIN;
+        int currMax = nums[0];
+        int currMin = nums[0];
+        int setBits = countBits(nums[0]);
+
+        for (size_t i = 1; i < nums.size(); i++) {
+
+            if (setBits == countBits(nums[i])) {
+                currMax = max(currMax, nums[i]);
+                currMin = min(currMin, nums[i]);
+
+            } else {
+
+                if (currMin < prevMax)
+                    return false;
+
+                prevMax = currMax;
+                setBits = countBits(nums[i]);
+                currMin = nums[i];
+                currMax = nums[i];
+            }
+        }
+
+        return currMin > prevMax;
+    }
+
+private:
+    int countBits(int num) { 
+        
+    return bitset<32>(num).count(); }
+};
