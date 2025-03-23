f83c2f2c506e80dc24448b23f5fa2dfa225dd0e1
Shahryar Hussain <123003299+Sshahryar@users.noreply.github.com>
2025-03-22 22:58:27 -0400

    Create Mar12,2025.cpp

---
+++ .LeetcodeDailySolution/Mar12,2025.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int maximumCount(std::vector<int>& nums) {
+
+        int n = nums.size();
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > 0) {
+                right = mid - 1;
+
+            } else {
+                left = mid + 1;
+            }
+        }
+        int positiveCount = n - left;
+        left = 0;
+        right = n - 1;
+
+        while (left <= right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < 0) {
+                left = mid + 1;
+
+            } else {
+                right = mid - 1;
+            }
+        }
+        int negativeCount = right + 1;
+
+        return max(positiveCount, negativeCount);
+    }
+};
